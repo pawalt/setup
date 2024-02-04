cf968d832d626addf4af7f15434b2af6c16c20f5
Peyton Walters <peyton@cockroachlabs.com>
2024-02-04 16:02:01 -0500

    ideavimrc

--- homes/common.nix
+++ homes/common.nix
@@ -327,5 +327,9 @@
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  home.file = {};
+  home.file = {
+    ".ideavimrc" = {
+      source = ./ideavimrc;
+    };
+  };
 }
